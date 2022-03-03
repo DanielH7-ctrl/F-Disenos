c60020195e41f13de4a5ca2f562ad26d6dfe2db7
DanielH7-ctrl <danielhuerta826@gmail.com>
2022-03-02 22:08:59 -0500

    Scroll parte 2

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -16,8 +16,47 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('Pagina 1'),
+    return Stack(
+      children: [
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+
+  Widget _textos() {
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox( height: 20.0 ),
+          Text('11°', style: estiloTexto ),
+          Text('Miércoles', style: estiloTexto ),
+          Expanded( child: Container() ),
+          Icon( Icons.keyboard_arrow_down, size: 70.0, color: Colors.white )
+        ],
+      ),
     );
   }
 
@@ -27,4 +66,5 @@ class ScrollPage extends StatelessWidget {
     );
   }
 
+  
 }
