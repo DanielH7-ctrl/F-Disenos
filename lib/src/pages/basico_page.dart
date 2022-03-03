854530c3052600f9fd99ba0db69a86a23e6a0c3d
DanielH7-ctrl <danielhuerta826@gmail.com>
2022-03-02 21:13:08 -0500

    Diseño parte 2

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -10,11 +10,25 @@ class BasicoPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Image(
-            image: NetworkImage('https://shotkit.com/wp-content/uploads/2020/08/night-landscape-photography-featured.jpg'),
-          ),
 
-          Container(
+          _crearImagen(),
+          _crearTitulo(),
+          _crearAcciones(),
+          _crearTexto(),
+          
+        ],
+      )
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+            image: NetworkImage('https://shotkit.com/wp-content/uploads/2020/08/night-landscape-photography-featured.jpg'),
+          );
+  }
+
+  Widget _crearTitulo() {
+    return Container(
             padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
             child: Row(
               children: [
@@ -28,15 +42,46 @@ class BasicoPage extends StatelessWidget {
                     ],
                   ),
                 ),
+
                 Icon(Icons.star, color: Colors.red, size: 30.0 ),
                 Text('41', style: TextStyle( fontSize: 20.0) )
 
               ],
             ),
-          )
-          
-        ],
-      )
+          );
+  }
+
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+
+        _accion( Icons.call, 'CALL'),
+        _accion( Icons.near_me, 'ROUTE'),
+        _accion( Icons.share, 'SHARE'),
+
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+
+    return Column(
+      children: [
+        Icon( icon, color: Colors.blue, size: 40.0 ),
+        SizedBox(height: 5.0 ),
+        Text( texto, style: TextStyle( fontSize: 15.0, color: Colors.blue ), )
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacinia bibendum risus, eget tempor diam facilisis a. Nam velit dui, eleifend a sem at, facilisis luctus enim. In hac habitasse platea dictumst. Vivamus egestas ac velit non lobortis. Suspendisse hendrerit elit nec tincidunt dapibus. Vestibulum id elit lorem. Suspendisse at tempus diam. Mauris sed dolor interdum mi ultrices sodales in ac diam.',
+        textAlign: TextAlign.justify,
+      ),
     );
   }
 }
